ee603b24fa06686698634d961bf198d9927030ac
Roller Wang <rwang@parasoft.com>
2023-04-04 13:06:37 +0800

    Set up enviroment for debugging from VSCode

--- PublishParasoftTestResults/publishtestresults.ts
+++ PublishParasoftTestResults/publishtestresults.ts
@@ -64,7 +64,7 @@ if (isNullOrWhitespace(searchFolder)) {
 
 let xUnitReports: string[] = [];
 let sarifReports: string[] = [];
-let matchingInputReportFiles: string[] = tl.findMatch(searchFolder, inputReportFiles);
+let matchingInputReportFiles: string[] = tl.findMatch(searchFolder || '', inputReportFiles);
 if (!matchingInputReportFiles || matchingInputReportFiles.length === 0) {
     tl.warning('No test result files matching ' + inputReportFiles + ' were found.');
     tl.setResult(tl.TaskResult.Succeeded, '');
