a731e5efeedad1c294f5f9d00d41be2990c509e3
tren <tren@parasoft.com>
2023-05-15 11:46:08 +0800

    Update unit test.

--- PublishParasoftTestResults/spec/ParaReportPublishService.spec.ts
+++ PublishParasoftTestResults/spec/ParaReportPublishService.spec.ts
@@ -1,6 +1,7 @@
 import * as tl from 'azure-pipelines-task-lib/task';
 import * as fs from 'fs';
 import * as SaxonJS from 'saxon-js';
+import {ParaReportPublishService} from "../ParaReportPublishService";
 
 let publisher: any;
 
@@ -13,31 +14,33 @@ describe("Parasoft findings Azure", () => {
         spyOn(tl, 'setResult');
         spyOn(tl, 'warning');
         spyOn(tl, 'getPathInput').and.returnValue(undefined);
-        // stop the script running
         spyOn(tl, 'findMatch').and.returnValue([]);
-        publisher = require('../publishtestresults');
 
+        publisher = new ParaReportPublishService();
     });
 
     describe('transformToSarif() - transform SA xml report to sarif report', () => {
         let sourcePath: string;
 
         beforeEach(() => {
+            sourcePath = './resources/dottest_SA_report.xml';
+
+            spyOn(publisher, 'transform').and.callThrough();
             spyOn(SaxonJS, 'transform').and.callThrough();
             spyOn(fs, 'writeFileSync').and.callThrough();
 
-            sourcePath = './resources/dottest_SA_report.xml';
-
+            expect(publisher.sarifReports.length).toBe(0);
+            expect(publisher.transform).not.toHaveBeenCalled();
         });
 
         it('when transform sarif report successfully', () => {
-            expect(publisher.sarifReports.length).toBe(0);
-
             publisher.transformToSarif(sourcePath);
             const jsonReport = fs.readFileSync(sourcePath + publisher.SARIF_SUFFIX, 'utf8');
             const report = JSON.parse(jsonReport);
 
             expect(publisher.sarifReports.length).toBe(1);
+            expect(publisher.transform).toHaveBeenCalled();
+            expect(publisher.transform).toHaveBeenCalledWith(sourcePath, publisher.SARIF_SEF_TEXT, sourcePath + publisher.SARIF_SUFFIX, publisher.sarifReports);
             expect(SaxonJS.transform).toHaveBeenCalled();
             expect(fs.writeFileSync).toHaveBeenCalled();
             expect(tl.warning).not.toHaveBeenCalledWith("Failed to transform report: " + sourcePath + ". See log for details.");
@@ -51,6 +54,9 @@ describe("Parasoft findings Azure", () => {
         it('when transform sarif report failed', () => {
             publisher.transformToSarif('not found');
 
+            expect(publisher.sarifReports.length).toBe(0);
+            expect(publisher.transform).toHaveBeenCalled();
+            expect(publisher.transform).toHaveBeenCalledWith('not found', publisher.SARIF_SEF_TEXT, 'not found' + publisher.SARIF_SUFFIX, publisher.sarifReports);
             expect(fs.writeFileSync).not.toHaveBeenCalled();
             expect(tl.warning).toHaveBeenCalledWith("Failed to transform report: not found. See log for details.");
         });
