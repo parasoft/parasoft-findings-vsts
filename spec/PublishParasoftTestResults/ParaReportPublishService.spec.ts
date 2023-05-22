a0f04a691155c08ab7bf4296d75fc93008c2569f
tren <tren@parasoft.com>
2023-05-22 15:40:12 +0800

    Optimize waiting strategy and change xml result from CRLF to LF.

--- spec/PublishParasoftTestResults/ParaReportPublishService.spec.ts
+++ spec/PublishParasoftTestResults/ParaReportPublishService.spec.ts
@@ -19,17 +19,26 @@ describe("Parasoft findings Azure", () => {
     });
 
     describe('transformReports() - transform report and input report type is', () => {
-        let waitTime: number;
+        let retryTimes: number;
 
         async function sleep(ms: number) {
             return new Promise(resolve => setTimeout(resolve, ms));
         }
 
+        async function waitForTransform(filePath: string) {
+            for (let i = 0; i < retryTimes; i++) {
+                if (fs.existsSync(filePath)){
+                    break;
+                }
+                await sleep(1000);
+            }
+        }
+
         beforeEach(() => {
             spyOn(publisher, 'transform').and.callThrough();
 
-            jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
-            waitTime = 10000;
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 200000;
+            retryTimes = 20;
 
             expect(publisher.sarifReports.length).toBe(0);
             expect(publisher.xUnitReports.length).toBe(0);
@@ -45,7 +54,7 @@ describe("Parasoft findings Azure", () => {
 
         it('XML_STATIC', async () => {
             publisher.transformReports([__dirname + '/resources/XML_STATIC.xml'], 0);
-            await sleep(waitTime);
+            await waitForTransform(__dirname + '/resources/XML_STATIC.xml-sast.sarif');
 
             let expectedReport = fs.readFileSync(__dirname + '/resources/expect/XML_STATIC.xml-sast.sarif', 'utf8');
             let result = fs.readFileSync(__dirname + '/resources/XML_STATIC.xml-sast.sarif', 'utf-8');
@@ -59,7 +68,7 @@ describe("Parasoft findings Azure", () => {
 
         it('XML_TESTS', async () => {
             publisher.transformReports([__dirname + '/resources/XML_TESTS.xml'], 0);
-            await sleep(waitTime);
+            await waitForTransform(__dirname + '/resources/XML_TESTS.xml-junit.xml');
 
             let expectedReport = fs.readFileSync(__dirname + '/resources/expect/XML_TESTS.xml-junit.xml', 'utf8');
             let result = fs.readFileSync(__dirname + '/resources/XML_TESTS.xml-junit.xml', 'utf-8');
@@ -73,7 +82,8 @@ describe("Parasoft findings Azure", () => {
 
         it('XML_STATIC_AND_TESTS', async () => {
             publisher.transformReports([__dirname + '/resources/XML_STATIC_AND_TESTS.xml'], 0);
-            await sleep(waitTime);
+            await waitForTransform(__dirname + '/resources/XML_STATIC_AND_TESTS.xml-sast.sarif');
+            await waitForTransform(__dirname + '/resources/XML_STATIC_AND_TESTS.xml-junit.xml');
 
             let expectedSarifReport = fs.readFileSync(__dirname + '/resources/expect/XML_STATIC_AND_TESTS.xml-sast.sarif', 'utf8');
             let sarifResult = fs.readFileSync(__dirname + '/resources/XML_STATIC_AND_TESTS.xml-sast.sarif', 'utf-8');
@@ -91,7 +101,7 @@ describe("Parasoft findings Azure", () => {
 
         it('XML_SOATEST', async () => {
             publisher.transformReports([__dirname + '/resources/XML_SOATEST.xml'], 0);
-            await sleep(waitTime);
+            await waitForTransform(__dirname + '/resources/XML_SOATEST.xml-junit.xml');
 
             let expectedReport = fs.readFileSync(__dirname + '/resources/expect/XML_SOATEST.xml-junit.xml', 'utf8');
             let result = fs.readFileSync(__dirname + '/resources/XML_SOATEST.xml-junit.xml', 'utf-8');
@@ -104,7 +114,7 @@ describe("Parasoft findings Azure", () => {
 
         it('XML_STATIC_AND_SOATEST', async () => {
             publisher.transformReports([__dirname + '/resources/XML_STATIC_AND_SOATEST.xml'], 0);
-            await sleep(waitTime);
+            await waitForTransform(__dirname + '/resources/XML_STATIC_AND_SOATEST.xml-sast.sarif');
 
             let expectedSarifReport = fs.readFileSync(__dirname + '/resources/expect/XML_STATIC_AND_SOATEST.xml-sast.sarif', 'utf8');
             let sarifResult = fs.readFileSync(__dirname + '/resources/XML_STATIC_AND_SOATEST.xml-sast.sarif', 'utf-8');
@@ -121,8 +131,8 @@ describe("Parasoft findings Azure", () => {
         });
 
         it('XML_XUNIT', async () => {
-            publisher.transformReports([__dirname + '/resources/expect/XML_XUNIT.xml'], 0);
-            await sleep(waitTime);
+            publisher.transformReports([__dirname + '/resources/XML_XUNIT.xml'], 0);
+            await sleep(5000);
 
             expect(publisher.transform).not.toHaveBeenCalled();
             expect(publisher.xUnitReports.length).toBe(1);
