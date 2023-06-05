fd2f202cf8672f556f22afb79396544b2821810f
tren <tren@parasoft.com>
2023-06-05 17:59:08 +0800

    Update based on feedback.

--- scripts/DownloadReportGenerator.ts
+++ scripts/DownloadReportGenerator.ts
@@ -1,20 +1,20 @@
-import * as DownloadService from './DownloadServices'
-import {IDownloadServices} from "./DownloadServices";
+import {DownloadReportGeneratorService} from './DownloadReportGeneratorService'
 import {AxiosRequestConfig} from '../PublishParasoftTestResults/node_modules/axios';
 
-
+// This script is used to download report generator from github and extract needed parts into PublishParasoftTestResults task
+// This lib is used to generate html coverage report by xml coverage report
 const tempFolder = './scripts/temp';
-const path = tempFolder + '/reportGenerator.zip';
-const targetFolder = './PublishParasoftTestResults/lib'
-const usedLibs = ['netcoreapp2.0/', 'net47/'];
-let options: AxiosRequestConfig<any> = {
+const pathToStore = tempFolder + '/reportGenerator.zip';
+const pathToExtract = './PublishParasoftTestResults/lib';
+const libsToUse = ['netcoreapp2.0/', 'net47/'];
+let downloadOptions: AxiosRequestConfig<any> = {
     url: 'https://github.com/danielpalme/ReportGenerator/releases/download/v4.6.1/ReportGenerator_4.6.1.zip',
     method: 'GET',
     responseType: 'stream'
 }
 
-let downloadService: IDownloadServices = new DownloadService.DownloadServices();
-downloadService.cleanDir([tempFolder, targetFolder]);
-downloadService.download(options, path, () => {
-    downloadService.extract(path, targetFolder, usedLibs);
-});
+let downloadService: DownloadReportGeneratorService = new DownloadReportGeneratorService();
+downloadService.cleanDir([tempFolder, pathToExtract]);
+downloadService.download(downloadOptions, pathToStore, () => {
+    downloadService.extract(pathToStore, pathToExtract, libsToUse);
+}).catch((error) =>{throw new Error(error)});
--- scripts/DownloadReportGeneratorService.ts
+++ scripts/DownloadReportGeneratorService.ts
@@ -3,28 +3,36 @@ import Axios from '../PublishParasoftTestResults/node_modules/axios';
 import {AxiosRequestConfig} from '../PublishParasoftTestResults/node_modules/axios';
 import * as AdmZip from 'adm-zip'
 
-export interface IDownloadServices {
-    download(option: AxiosRequestConfig<any>, path: string, callback: any): void;
-    cleanDir(paths: string[]): void;
-    extract(zipPath: string, targetDir: string, usedLibs: string[]): void;
-}
+export class DownloadReportGeneratorService {
 
-export class DownloadServices implements IDownloadServices {
-
-    download = (option: AxiosRequestConfig<any>, path: string, callback: any): void => {
-        Axios(option).then(res => {
+    download = (option: AxiosRequestConfig<any>, pathToStore: string, callback: any): Promise<any> => {
+        return Axios(option).then(res => {
             if (res.status == 200) {
-                res.data.pipe(fs.createWriteStream(path));
                 res.data.on("end", () => {
                     console.log("Report Generator: download completed");
                     callback();
                 });
+                const writer = fs.createWriteStream(pathToStore);
+                return new Promise((resolve, reject) => {
+                    res.data.pipe(writer);
+                    let error: any = null;
+                    writer.on('error', err => {
+                        error = err;
+                        writer.close();
+                        reject(err);
+                    });
+                    writer.on('close', () => {
+                        if (!error) {
+                            resolve(true);
+                        }
+                    });
+                });
             } else {
-                console.log(`Error: ` + res.status);
+                return Promise.reject(`Download failed - ` + res.status);
             }
-        }).catch(err => {
-            console.log(err);
-        })
+        }).catch((error) => {
+            return Promise.reject(`Download failed - ` + error.message);
+        });
     }
 
     cleanDir = (paths: string[]): void => {
@@ -32,9 +40,10 @@ export class DownloadServices implements IDownloadServices {
             if (fs.existsSync(paths[i])) {
                 try {
                     fs.rmSync(paths[i], {recursive: true});
-                    console.log(`Removed ${paths[i]}`)
+                    console.log(`Removed ${paths[i]}`);
                 } catch (e) {
                     console.error(`Error removing ${paths[i]}:`, e);
+                    throw(e);
                 }
             }
             try {
@@ -42,6 +51,7 @@ export class DownloadServices implements IDownloadServices {
                 console.log(`Created directory ${paths[i]}`);
             } catch (e) {
                 console.error(`Error creating directory ${paths[i]}:`, e);
+                throw(e);
             }
         }
     }
@@ -49,7 +59,7 @@ export class DownloadServices implements IDownloadServices {
     extract = (zipPath: string, targetDir: string, usedLibs: string[]): void => {
         const zip = new AdmZip(zipPath);
         usedLibs.forEach((lib) => {
-            zip.extractEntryTo(lib, targetDir)
+            zip.extractEntryTo(lib, targetDir);
         });
         console.log("Report Generator: Extract completed");
     }
