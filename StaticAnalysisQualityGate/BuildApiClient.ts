b0a8766ad5fc767df6318df2daaf888ee16f3b7b
Renee Liu <rliu@parasoft.com>
2023-10-12 14:14:20 +0800

    Add copy right comment

--- StaticAnalysisQualityGate/BuildApiClient.ts
+++ StaticAnalysisQualityGate/BuildApiClient.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Parasoft Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import * as tl from 'azure-pipelines-task-lib/task';
 import * as azdev from "azure-devops-node-api";
 import * as BuildApi from "azure-devops-node-api/BuildApi";
@@ -18,6 +33,7 @@ export enum FileSuffixEnum {
     // COBERTURA_SUFFIX = "-cobertura.xml"
 }
 
+/* This file is a copy of PublishParasoftResults/BuildApiClient.ts */
 export class BuildAPIClient {
     private readonly accessToken: string;
     private readonly buildApi: Promise<BuildApi.IBuildApi>;
