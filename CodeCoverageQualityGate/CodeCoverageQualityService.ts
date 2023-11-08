7d08b688c25df262dd6fdb6961157703ab0c1c40
Renee Liu <rliu@parasoft.com>
2023-11-08 15:27:56 +0800

    Optimized code

--- CodeCoverageQualityGate/CodeCoverageQualityService.ts
+++ CodeCoverageQualityGate/CodeCoverageQualityService.ts
@@ -19,17 +19,12 @@ import { BuildAPIClient, DefaultBuildReportResults, DefaultBuildReportResultsSta
 
 interface ReferenceBuildResult {
     originalPipelineName: string,
-    originalBuildNumber: string,
+    originalBuildNumber: string
 }
 
-interface ReferenceBuildInformation {
-    fileEntries: FileEntry[],
-    codeCoverage: {
-        pipelineName: string | undefined,
-        buildId: number | undefined,
-        buildNumber:  string | undefined,
-        warningMessage: string | undefined
-    },
+interface BuildInformation {
+    fileEntry: FileEntry | undefined,
+    warningMessage: string | undefined,
     isDebugMessage: boolean
 }
 
@@ -65,10 +60,6 @@ export class CodeCoverageQualityService {
 
     originalReferencePipelineName: string | undefined;
     originalReferenceBuildNumber: string | undefined;
-    referencePipelineName: string | undefined;
-    referenceBuildNumber: string | undefined;
-    referenceBuildId: string | undefined;
-    referenceBuildWarningMessage: string | undefined;
 
     constructor() {
         this.projectName = tl.getVariable('System.TeamProject') || '';
@@ -133,61 +124,78 @@ export class CodeCoverageQualityService {
 
     run = async (): Promise<void> => {
         try {
-            // To get Cobertura report in current build
-            // Check for coverage results exist in current build
-            const currentBuildArtifact: BuildArtifact = await this.buildClient.getBuildArtifact(this.projectName, Number(this.buildId), this.COBERTURA_ARTIFACT_NAME);
-            if (!currentBuildArtifact) {
-                tl.warning(`Quality gate '${this.getQualityGateIdentification()}' was skipped; no Parasoft cobertura results were found in this build`);
-                return;
-            }
-            const fileEntries = await this.buildClient.getBuildReportsWithId(currentBuildArtifact, Number(this.buildId), this.fileSuffix);
-            if (fileEntries.length == 0) { // When current cobertura report does not exist
-                tl.warning(`Quality gate '${this.getQualityGateIdentification()}' was skipped; no Parasoft cobertura results were found in this build`);
-                return;
-            }
-            const currentCoberturaReport: FileEntry = fileEntries[0];
-
-            // To get Cobertura report in reference build
             let codeCoverageReferenceBuild = tl.getVariable('PF.ReferenceBuildResult');
             if (!codeCoverageReferenceBuild) {
-                tl.warning(`Quality gate '${this.getQualityGateIdentification()}' was skipped: please run 'Publish Parasoft Results' task first`);
+                tl.setResult(tl.TaskResult.SucceededWithIssues, `Quality gate '${this.getQualityGateIdentification()}' was skipped; please run 'Publish Parasoft Results' task first`);
                 return;
             }
             let referenceBuild: ReferenceBuildResult = JSON.parse(<string> codeCoverageReferenceBuild);
             this.originalReferencePipelineName = referenceBuild.originalPipelineName;
             this.originalReferenceBuildNumber = referenceBuild.originalBuildNumber;
 
-            const referenceCoberturaReport: FileEntry = await this.getReferenceCoberturaReport();
+            // To get Cobertura report in current build
+            const currentBuildInformation: BuildInformation = await this.getCurrentBuildInformation();
+            if (currentBuildInformation.warningMessage) {
+                if (currentBuildInformation.isDebugMessage) {
+                    tl.debug(`${currentBuildInformation.warningMessage}`);
+                } else {
+                    tl.setResult(tl.TaskResult.SucceededWithIssues, `Quality gate '${this.getQualityGateIdentification()}' was skipped; ${currentBuildInformation.warningMessage}`);
+                    return;
+                }
+            }
+            const currentCoberturaReport = currentBuildInformation.fileEntry;
 
-            // TODO (CICD-532)Calculate modified code coverage
-            // TODO The following four lines can be removed in CICD-532, just used to verify whether to get the cobertura report
-            let currentCoberturaContentString: string = await currentCoberturaReport.contentsPromise;
-            console.log("currentCoberturaContentString::: " + currentCoberturaContentString);
-            let referenceCoberturaContentString: string = await referenceCoberturaReport.contentsPromise;
-            console.log("referenceCoberturaContentString::: " + referenceCoberturaContentString);
+            if (this.type == TypeEnum.OVERALL) {
+                // TODO (CICD-533) Implement the function of quality gate - support total type
+            } else if (this.type == TypeEnum.MODIFIED) {
+                // To get Cobertura report in reference build
+                const referenceBuildInformation: BuildInformation = await this.getReferenceBuildInformation();
+                if (referenceBuildInformation.warningMessage) {
+                    if (referenceBuildInformation.isDebugMessage) {
+                        tl.debug(`${referenceBuildInformation.warningMessage}`);
+                    } else {
+                        tl.setResult(tl.TaskResult.SucceededWithIssues, `Quality gate '${this.getQualityGateIdentification()}' was skipped; ${referenceBuildInformation.warningMessage}`);
+                        return;
+                    }
+                }
+                const referenceCoberturaReport = referenceBuildInformation.fileEntry;
+                // TODO (CICD-534)Calculate modified code coverage
+            }
         } catch(error) {
             tl.warning(`Failed to process the quality gate '${this.getQualityGateIdentification()}'. See logs for details.`);
             console.error(error);
             return;
         }
-            
-        tl.debug('originalReferencePipelineName: ' + this.originalReferencePipelineName);
-        tl.debug('originalReferenceBuildNumber: ' + this.originalReferenceBuildNumber);
     }
 
-    private getReferenceCoberturaReport = async (): Promise<FileEntry> => {
-        let referenceBuildInfo: ReferenceBuildInformation = {
-            fileEntries: [],
-            codeCoverage: {
-                pipelineName: undefined,
-                buildId: undefined,
-                buildNumber: undefined,
-                warningMessage: undefined
-            },
+    private getCurrentBuildInformation = async (): Promise<BuildInformation> => {
+        let currentBuildInfo: BuildInformation = {
+            fileEntry: undefined,
+            warningMessage: undefined,
+            isDebugMessage: false
+        };
+        const currentBuildArtifact: BuildArtifact = await this.buildClient.getBuildArtifact(this.projectName, Number(this.buildId), this.COBERTURA_ARTIFACT_NAME);
+        if (!currentBuildArtifact) {
+            currentBuildInfo.warningMessage = "no Parasoft coverage results were found in this build";
+            return currentBuildInfo;
+        }
+        const fileEntries = await this.buildClient.getBuildReportsWithId(currentBuildArtifact, Number(this.buildId), this.fileSuffix);
+        if (fileEntries.length == 0) {
+            currentBuildInfo.warningMessage = "no Parasoft coverage results were found in this build";
+            return currentBuildInfo;
+        }
+        currentBuildInfo.fileEntry = fileEntries[0]; // Only get the first report since there is only one cobertura report in Azure Artifact
+        return currentBuildInfo;
+    }
+
+    private getReferenceBuildInformation = async (): Promise<BuildInformation> => {
+        let referenceBuildInfo: BuildInformation = {
+            fileEntry: undefined,
+            warningMessage: undefined,
             isDebugMessage: false
         };
         if (this.originalReferencePipelineName == this.pipelineName && this.originalReferenceBuildNumber == this.buildNumber) {
-            tl.warning('Using the current build as the reference');
+            referenceBuildInfo.warningMessage = "the current build is not allowed to use as the reference";
         } else {
             if (!this.originalReferencePipelineName) { // Reference pipeline is not specified
                 tl.debug("No reference pipeline has been set; using the current pipeline as reference.");
@@ -201,55 +209,40 @@ export class CodeCoverageQualityService {
                     let specificReferencePipelineId : number = Number(specificReferencePipeline.id);
                     referenceBuildInfo = await this.getBuildsForSpecificPipeline(specificReferencePipelineId, specificReferencePipeline.name || '');
                 } else if (specificPipelines.length > 1) {
-                    referenceBuildInfo.codeCoverage.warningMessage = `The specified reference pipeline '${this.originalReferencePipelineName}' is not unique`;
+                    referenceBuildInfo.warningMessage = `the specified reference pipeline '${this.originalReferencePipelineName}' is not unique`;
                 } else {
-                    referenceBuildInfo.codeCoverage.warningMessage = `The specified reference pipeline '${this.originalReferencePipelineName}' could not be found`;
+                    referenceBuildInfo.warningMessage = `the specified reference pipeline '${this.originalReferencePipelineName}' could not be found`;
                 }
             }
         }
-        if (referenceBuildInfo.codeCoverage.warningMessage) {
-            if (referenceBuildInfo.isDebugMessage) {
-                tl.debug(`${referenceBuildInfo.codeCoverage.warningMessage} - type of code coverage calculation will use default value Overall`);
-            } else {
-                tl.warning(`${referenceBuildInfo.codeCoverage.warningMessage} - type of code coverage calculation will use default value Overall`);
-            }
-            referenceBuildInfo.codeCoverage.warningMessage = referenceBuildInfo.codeCoverage.warningMessage + ' - type of code coverage calculation used default value Overall';
-        }
-        return Promise.resolve(referenceBuildInfo.fileEntries[0]);
+        return referenceBuildInfo;
     }
 
-    private async getBuildsForSpecificPipeline(specificReferencePipelineId: number, pipelineName: string): Promise<ReferenceBuildInformation> {
-        const referenceBuildInfo: ReferenceBuildInformation = {
-            fileEntries: [],
-            codeCoverage: {
-                pipelineName: pipelineName,
-                buildId: undefined,
-                buildNumber: undefined,
-                warningMessage: undefined
-            },
+    private async getBuildsForSpecificPipeline(specificReferencePipelineId: number, pipelineName: string): Promise<BuildInformation> {
+        const referenceBuildInfo: BuildInformation = {
+            fileEntry: undefined,
+            warningMessage: undefined,
             isDebugMessage: false
         };
         const allBuildsForSpecificPipeline = await this.buildClient.getBuildsForSpecificPipeline(this.projectName, specificReferencePipelineId);
         if (!this.originalReferenceBuildNumber) { // Reference build is not specified
             tl.debug(`No reference build has been set; using the last successful build in pipeline '${pipelineName}' as reference.`);
-            let defaultBuildReportResults: DefaultBuildReportResults = await this.buildClient.getDefaultBuildReports(allBuildsForSpecificPipeline, this.projectName, this.COBERTURA_ARTIFACT_NAME, FileSuffixEnum.COBERTURA_SUFFIX, this.buildId);
+            let defaultBuildReportResults: DefaultBuildReportResults = await this.buildClient.getDefaultBuildReports(allBuildsForSpecificPipeline, this.projectName, this.COBERTURA_ARTIFACT_NAME, this.fileSuffix, this.buildId);
             switch (defaultBuildReportResults.status) {
                 case DefaultBuildReportResultsStatus.OK:
-                    referenceBuildInfo.fileEntries = defaultBuildReportResults.reports || [];
+                    referenceBuildInfo.fileEntry = defaultBuildReportResults.reports?.at(0);
                     tl.debug(`Set build '${pipelineName}#${defaultBuildReportResults.buildNumber}' as the default reference build`);
-                    referenceBuildInfo.codeCoverage.buildId = defaultBuildReportResults.buildId;
-                    referenceBuildInfo.codeCoverage.buildNumber = defaultBuildReportResults.buildNumber;
                     return referenceBuildInfo;
                 case DefaultBuildReportResultsStatus.NO_PARASOFT_RESULTS_IN_PREVIOUS_SUCCESSFUL_BUILDS:
-                    referenceBuildInfo.codeCoverage.warningMessage = `No Parasoft coverage results were found in any of the previous successful builds in pipeline '${pipelineName}'`;
+                    referenceBuildInfo.warningMessage = `no Parasoft coverage results were found in any of the previous successful builds in pipeline '${pipelineName}'`;
                     return referenceBuildInfo;
                 case DefaultBuildReportResultsStatus.NO_PREVIOUS_BUILD_WAS_FOUND:
-                    referenceBuildInfo.codeCoverage.warningMessage = `No previous build was found in pipeline '${pipelineName}'`;
+                    referenceBuildInfo.warningMessage = `No previous build was found in pipeline '${pipelineName}'`;
                     referenceBuildInfo.isDebugMessage = true;
                     return referenceBuildInfo;
                 case DefaultBuildReportResultsStatus.NO_SUCCESSFUL_BUILD:
                 default:
-                    referenceBuildInfo.codeCoverage.warningMessage = `No successful build was found in pipeline '${pipelineName}'`;
+                    referenceBuildInfo.warningMessage = `no successful build was found in pipeline '${pipelineName}'`;
                     return referenceBuildInfo;
             }
         } else { // Reference build is specified
@@ -258,12 +251,12 @@ export class CodeCoverageQualityService {
             });
 
             if (specificReferenceBuilds.length > 1) {
-                referenceBuildInfo.codeCoverage.warningMessage = `The specified reference build '${pipelineName}#${this.originalReferenceBuildNumber}' is not unique`;
+                referenceBuildInfo.warningMessage = `the specified reference build '${pipelineName}#${this.originalReferenceBuildNumber}' is not unique`;
                 return referenceBuildInfo;
             }
 
             if (specificReferenceBuilds.length == 0) {
-                referenceBuildInfo.codeCoverage.warningMessage = `The specified reference build '${pipelineName}#${this.originalReferenceBuildNumber}' could not be found`;
+                referenceBuildInfo.warningMessage = `the specified reference build '${pipelineName}#${this.originalReferenceBuildNumber}' could not be found`;
                 return referenceBuildInfo;
             }
 
@@ -271,7 +264,7 @@ export class CodeCoverageQualityService {
             const specificReferenceBuild = specificReferenceBuilds[0];
             // Check for the succeeded or paratially-succeeded results exist in the specific reference build
             if (specificReferenceBuild.result != BuildResult.Succeeded && specificReferenceBuild.result != BuildResult.PartiallySucceeded) {
-                referenceBuildInfo.codeCoverage.warningMessage = `The specified reference build '${pipelineName}#${this.originalReferenceBuildNumber}' cannot be used. Only successful or unstable builds are valid references`;
+                referenceBuildInfo.warningMessage = `the specified reference build '${pipelineName}#${this.originalReferenceBuildNumber}' cannot be used. Only successful or unstable builds are valid references`;
                 return referenceBuildInfo;
             }
 
@@ -279,20 +272,17 @@ export class CodeCoverageQualityService {
             // Check for Parasoft results exist in the specific reference build
             const artifact: BuildArtifact = await this.buildClient.getBuildArtifact(this.projectName, specificReferenceBuildId, this.COBERTURA_ARTIFACT_NAME);
             if (!artifact) {
-                referenceBuildInfo.codeCoverage.warningMessage = `No Parasoft coverage results were found in the specified reference build: '${pipelineName}#${this.originalReferenceBuildNumber}'`;
+                referenceBuildInfo.warningMessage = `no Parasoft coverage results were found in the specified reference build: '${pipelineName}#${this.originalReferenceBuildNumber}'`;
                 return referenceBuildInfo;
             }
 
-            referenceBuildInfo.fileEntries = await this.buildClient.getBuildReportsWithId(artifact, specificReferenceBuildId, FileSuffixEnum.COBERTURA_SUFFIX);
-
-            if (referenceBuildInfo.fileEntries.length != 1) { // There will be only one cobertura report in Azure Artifact
-                referenceBuildInfo.codeCoverage.warningMessage = `No Parasoft coverage results were found in the specified reference build: '${pipelineName}#${this.originalReferenceBuildNumber}'`;
+            const fileEntries = await this.buildClient.getBuildReportsWithId(artifact, specificReferenceBuildId, this.fileSuffix);
+            if (fileEntries.length == 0) {
+                referenceBuildInfo.warningMessage = `no Parasoft coverage results were found in the specified reference build: '${pipelineName}#${this.originalReferenceBuildNumber}'`;
                 return referenceBuildInfo;
             }
-
             tl.debug(`Retrieved Parasoft coverage results from the reference build '${pipelineName}#${this.originalReferenceBuildNumber}'`);
-            referenceBuildInfo.codeCoverage.buildId = specificReferenceBuildId;
-            referenceBuildInfo.codeCoverage.buildNumber = this.originalReferenceBuildNumber;
+            referenceBuildInfo.fileEntry = fileEntries[0]; // Only get the first report since there is only one cobertura report in Azure Artifact
             return referenceBuildInfo;
         }
     }
