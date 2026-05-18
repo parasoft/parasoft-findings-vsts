60f86c949e82f69f307a171a866b5db89c2203e5
tren <tren@parasoft.com>
2026-05-18 10:18:11 +0800

    Fix eslint violation

--- scripts/DownloadReportGenerator.ts
+++ scripts/DownloadReportGenerator.ts
@@ -7,6 +7,7 @@ const tempFolder = './scripts/temp';
 const pathToStore = tempFolder + '/reportGenerator.zip';
 const pathToExtract = './PublishParasoftResults/lib';
 const libsToUse = ['netcoreapp2.0/', 'net47/'];
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const downloadOptions: AxiosRequestConfig<any> = {
     url: 'https://github.com/danielpalme/ReportGenerator/releases/download/v4.6.1/ReportGenerator_4.6.1.zip',
     method: 'GET',
